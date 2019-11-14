b0ea16ddef6f158fb9082bf3164b3d47c67a6cd5
nakayuji <xiongshizhongsi@gmail.com>
2019-11-14 13:12:21 +0900

    クイズクラス完成

--- src/main/java/com/example/newquizapp/NewQuizAppController.java
+++ src/main/java/com/example/newquizapp/NewQuizAppController.java
@@ -1,16 +1,27 @@
 package com.example.newquizapp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class NewQuizAppController {
+    private List<Quiz> quizzes = new ArrayList<>();
 
     @GetMapping("/show")
-    public String show(){
-        return "Hello World";
+    public List<Quiz> show() {
+        return quizzes;
+    }
+
+    @PostMapping("/create")
+    public void create(@RequestParam String question, @RequestParam boolean answer){
+        Quiz quiz = new Quiz(question, answer);
+        quizzes.add(quiz);
     }
 }
 
 
-//テスト
