43d40f7df764af3af678a00dc54dfca78b6ce646
nakayuji <xiongshizhongsi@gmail.com>
2019-11-14 16:16:08 +0900

    問題を登録できる

--- src/main/java/com/example/newquizapp/Quiz.java
+++ src/main/java/com/example/newquizapp/Quiz.java
@@ -23,4 +23,17 @@ public class Quiz {
     public boolean isAnswer() {
         return answer;
     }
+
+    @Override
+    public String toString() {
+        String marubatsu = answer ? "◯" : "×";
+        return question + " " + marubatsu;
+    }
+
+    public static Quiz fromString(String line) {
+        String question = line.substring(0, line.length() - 2);
+        boolean answer = line.endsWith("◯");
+
+        return new Quiz(question, answer);
+    }
 }
